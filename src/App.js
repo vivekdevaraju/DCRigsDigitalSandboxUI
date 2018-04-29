225fe6a8cc10a9a135304855b15d1b82148738bc
vivekdevaraju <vivekdevaraju@gmail.com>
2018-04-29 14:35:43 +0530

    CSS Modules + Eject

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
   state = {
     persons :[
-      {id:'1',name : "Vivek", age :30},
-      {id:'2',name : "Divya", age :29},
-      {id:'3',name : "Krishna", age :29}
+      {id:'1',name : 'Vivek', age :30},
+      {id:'2',name : 'Divya', age :29},
+      {id:'3',name : 'Krishna', age :29}
     ],
     showPersons : false
   }
@@ -18,11 +18,11 @@ class App extends Component {
      person.name = event.target.value;
      const persons = [...this.state.persons];
      persons[personIndex] = person;
-    this.setState({persons :persons})
+    this.setState({persons :persons});
   }
 
   deletePersonHandler = (personIndex) => {
-    //const persons = this.state.persons.slice();
+    // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
     persons.splice(personIndex,1);
     this.setState({persons:persons});
@@ -32,46 +32,39 @@ class App extends Component {
     const currentState = this.state.showPersons;
     this.setState({
       showPersons : !currentState
-    })
+    });
   }
   render() {
-    const buttonStyle = {
-      backgroundColor : 'green',
-      color :'white',
-      font : 'inherit',
-      border : '1px solid blue',
-      padding : '8px',
-      cursor : 'pointer'
-    }
 
     let persons = null;
-    if(this.state.showPersons){
+    let btnClass = '';
+    if (this.state.showPersons){
            persons = (
         <div>
           {this.state.persons.map((person,index) => {
             return <Person name = {person.name} age = {person.age}
-            click ={() => {this.deletePersonHandler(index)}}
+            click ={() => {this.deletePersonHandler(index);}}
             key = {person.id}
-            changed = {(event) => {this.nameChangeHandler(event,person.id)}}/>
+            changed = {(event) => {this.nameChangeHandler(event,person.id);}}/>;
           })}       
      </div>
       );
-      buttonStyle.backgroundColor = 'red';
-     
+      
+      btnClass = classes.Red;
     }
-    const classes = [];
-    if(this.state.persons.length <= 2){
-      classes.push('red');
+    const assignedClasses = [];
+    if (this.state.persons.length <= 2){
+      assignedClasses.push(classes.red);
     }
-    if(this.state.persons.length <= 1){
-      classes.push('bold');
+    if (this.state.persons.length <= 1){
+      assignedClasses.push(classes.bold);
     }
      return (
-    <div className="App">
+    <div className={classes.App}>
      <h1> Hi, </h1>
-     <p className = {classes.join(' ')}>This is really working</p>
+     <p className = {assignedClasses.join(' ')}>This is really working</p>
 
-     <button style = {buttonStyle} onClick = {this.togglePersonHanlder}>Toggle Persons </button>
+     <button className = {btnClass} onClick = {this.togglePersonHanlder}>Toggle Persons </button>
      {persons}
     </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
-import './Person.css';
+import classes from './Person.css';
 
 
-const person = (props) => {
-    
-    return (<div className ="Person" >
+const person = (props) => {   
+    return (<div className = {classes.Person} >
         <p onClick = {props.click}>I'm a Person and My name is {props.name} I am {props.age} Old.</p>
         <p>{props.children}</p>
         <input type="text" onChange = {props.changed} value = {props.name}/>
